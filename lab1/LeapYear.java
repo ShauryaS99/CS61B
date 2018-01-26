388fcf4db3c4160277a79f7371e68a22572707f0
Shaurya Sanghvi <shaurya_s@berkeley.edu>
2018-01-26 12:17:31 -0800

    final product

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -8,7 +8,7 @@ public class LeapYear {
         if (year%4 !=0){
             return false;
         }
-        if (year%400 !=0) {
+        if (year%100 ==0 && year%400 !=0) {
             return false;
         }
         else{
