a120624599b2fee3fc67689998cfa41a63b59fd2
Shaurya Sanghvi <shaurya_s@berkeley.edu>
2018-01-26 23:17:14 -0800

    final version proj0

--- proj0/Planet.java
+++ proj0/Planet.java
@@ -5,7 +5,7 @@ public class Planet{
 	public double yyVel;
 	public double mass;
 	public String imgFileName;
-	static final double gravity = 6.67e-11;
+	private static final double gravity = 6.67e-11;
 
 	public Planet(double xP, double yP, double xV,
               double yV, double m, String img){
@@ -91,4 +91,5 @@ public class Planet{
 	public void draw(){
 		StdDraw.picture(this.xxPos,this.yyPos,"images/" + this.imgFileName);
 	}
+
 }
